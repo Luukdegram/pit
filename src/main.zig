947c7f839c2d351d58c29fd8d5244aedea985240
Luuk de Gram <Luukdegram@users.noreply.github.com>
2020-12-28 14:58:19 +0100

    Parse args

--- src/main.zig
+++ src/main.zig
@@ -4,5 +4,21 @@ const editor = @import("editor.zig");
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
-    try editor.run(&gpa.allocator, null);
+
+    const alloc = &gpa.allocator;
+
+    var it = std.process.args();
+
+    // skip first arg as it contains the executable
+    const exe = it.next(alloc);
+    alloc.free(try exe.?);
+
+    const possible_path = it.next(alloc);
+    var path: ?[]const u8 = null;
+
+    if (possible_path) |p| path = try p;
+    defer if (path) |p| alloc.free(p);
+
+    // finally, run the editor
+    try editor.run(alloc, path);
 }
